2f732ecf92e6e814a5e2b264263e95a345432139
shoko.kobayashi <shoko.kobayashi@gmail.com>
2019-06-02 12:39:54 +0900

    情報画面作成

--- Count30/Count30/Count30/ViewModels/ViewInfoViewModel.cs
+++ Count30/Count30/Count30/ViewModels/ViewInfoViewModel.cs
@@ -1,15 +1,53 @@
-﻿using Prism.Navigation;
+﻿using Prism.Commands;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Count30.ViewModels
 {
     public class ViewInfoViewModel : ViewModelBase
     {
+        private string _twitterId = "@shoko_kb";
+        private string _url = "https://shoko.mitose.jp/";
+        private string _twitterUrl = $"https://twitter.com/shoko_kb";
+
+        public ICommand AccessWebSiteCommand { get; }
+        public ICommand AccessTwitterCommand { get; }
+
+        public string TwitterId 
+        {
+            get => _twitterId;
+            set => SetProperty(ref _twitterId, value);
+        }
+        public string Url
+        {
+            get => _url;
+            set => SetProperty(ref _url, value);
+        }
+
+        public string TwitterUrl
+        {
+            get => _twitterUrl;
+            set => SetProperty(ref _twitterUrl, value);
+        }
+
         public ViewInfoViewModel(INavigationService navigationService) : base(navigationService)
         {
             Title = "情報";
+
+
+            AccessWebSiteCommand = new DelegateCommand(() =>
+            {
+                Device.OpenUri(new Uri(Url));
+            });
+
+            AccessTwitterCommand = new DelegateCommand(() =>
+            {
+                Device.OpenUri(new Uri(TwitterUrl));
+            });
         }
     }
 }
